32030a2c534d80f4d119a05051459908bb078e8f
Filipe Cândido <filipecandido123@hotmail.com>
2022-08-18 19:30:37 -0300

    feat: add validate login factory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { MakeSignUpController } from '../factories/signup'
 import { adaptRoute } from '../adapters/express-routes-adapter'
+import { MakeSignUpController } from '../factories/signup/signup'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(MakeSignUpController()))
