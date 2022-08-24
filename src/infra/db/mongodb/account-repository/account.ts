95eb0478d9b1ab7bfb139ea66a9a4a99f4f22add
Filipe Cândido <filipecandido123@hotmail.com>
2022-08-23 23:52:48 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,23 @@
+import type { WithId, Document } from 'mongodb'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { addAccountModel } from '../../../../domain/usecases/addAccount'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
-  async add(acountData: addAccountModel): Promise<AccountModel> {
+interface AccountWithId extends WithId<Document>, AccountModel {}
+
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+  async add(accountData: addAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(acountData)
-    const { insertedId, ...collectionWithOutId } = result
-    return Object.assign({}, collectionWithOutId, { id: insertedId }) as unknown as AccountModel
+    const result = await accountCollection.insertOne(accountData)
+    const { insertedId, ...rest } = result
+    return Object.assign({}, rest, { id: insertedId }) as unknown as AccountModel
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email }) as AccountWithId
+    return account as unknown as AccountModel
   }
 }
