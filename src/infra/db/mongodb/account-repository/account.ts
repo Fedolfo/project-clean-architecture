0b3286504f03e465762761f9f3b3470f3ea4a792
Filipe Cândido <filipecandido123@hotmail.com>
2022-08-23 23:54:02 -0300

    --amend--no-edit

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,11 +1,8 @@
-import type { WithId, Document } from 'mongodb'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { addAccountModel } from '../../../../domain/usecases/addAccount'
 import { MongoHelper } from '../helpers/mongo-helper'
-
-interface AccountWithId extends WithId<Document>, AccountModel {}
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: addAccountModel): Promise<AccountModel> {
@@ -17,7 +14,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
   async loadByEmail (email: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ email }) as AccountWithId
+    const account = await accountCollection.findOne({ email })
     return account as unknown as AccountModel
   }
 }
